2457e80d59e5e85c4d7186843eb8063fb88db683
Joanna Semeniuk <semeniuk.asia@gmail.com>
2022-02-02 18:47:28 +0100

    Add staging server to functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,5 @@
+import os
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
@@ -10,6 +12,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
     
     def setUp(self):
         self.browser = webdriver.Chrome()
+        staging_server = os.environ.get('STAGING_SERVER')  
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.quit()
